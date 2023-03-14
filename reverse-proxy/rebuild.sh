b4e58d5686beb855659ea361b5504b1e8d1eb332
Finlay Maroney <finman292004@protonmail.com>
2023-03-13 21:51:50 -0400

    Added certs dir gitignore and fixed reverse proxy setup

--- reverse-proxy/rebuild.sh
+++ reverse-proxy/rebuild.sh
@@ -3,5 +3,5 @@ export NAME=reverse-proxy
 docker build -t cses/$NAME .
 docker stop $NAME
 docker rm $NAME
-docker run -it -d --restart unless-stopped --name $NAME --net httpd-net -v /var/run/docker.sock:/tmp/docker.sock:ro cses/$NAME
+docker run -it -d --restart unless-stopped --name $NAME --net httpd-net -p 80:80 -p 443:443 -v certs:/etc/nginx/certs:ro -v /var/run/docker.sock:/tmp/docker.sock:ro cses/$NAME
 docker network connect bridge $NAME
